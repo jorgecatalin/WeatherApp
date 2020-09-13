2b9a7c04c010090c17d93eda27a8cb7e88912493
jorgecatalin <jorge_catalin@yahoo.com>
2020-09-13 15:56:53 +0300

    c

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react"
+import "./App.css"
 function App() {
+  useEffect(() => {
+    let longitudine = 0,
+      latitudine = 0
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        longitudine = position.coords.longitude
+        latitudine = position.coords.latitude
+      })
+    } else console.log("NU MERGE GEOLOCATIA ")
+    console.log(longitudine, latitudine)
+
+    let requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    }
+    let fetchText =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      latitudine +
+      "&lon=" +
+      longitudine +
+      "&exclude=hourly,daily&appid=e9e74eee0dd11008febcccb305a879fe"
+    fetch(fetchText, requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error))
+    console.log("merge")
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>af</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
