992b30724d38573493e788805855a8805ee3a93d
jorgecatalin <jorge_catalin@yahoo.com>
2020-09-14 00:03:02 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import React, { useState, useEffect } from "react"
-import "./App.css"
+import Navbar from "./Componente/Navbar.js"
+import Content from "./Componente/Content.js"
+import Footer from "./Componente/Footer.js"
+
 function App() {
+  const [date, setDate] = useState({})
+  const [wait, setWait] = useState(true)
   useEffect(() => {
-    let longitudine = 0,
-      latitudine = 0
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        longitudine = position.coords.longitude
-        latitudine = position.coords.latitude
+        let longitudine = position.coords.longitude,
+          latitudine = position.coords.latitude
+        console.log(longitudine, latitudine)
+        let requestOptions = {
+          method: "GET",
+          redirect: "follow",
+        }
+        let fetchText =
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          latitudine +
+          "&lon=" +
+          longitudine +
+          "&appid=e9e74eee0dd11008febcccb305a879fe&units=metric&exclude=hourly,minutely&lang=ro"
+        fetch(fetchText, requestOptions)
+          .then((response) => response.json())
+          .then((result) => {
+            console.log(result)
+            setDate(result)
+            setWait(false)
+          })
+          .catch((error) => console.log("error", error))
       })
     } else console.log("NU MERGE GEOLOCATIA ")
-    console.log(longitudine, latitudine)
-
-    let requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    }
-    let fetchText =
-      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-      latitudine +
-      "&lon=" +
-      longitudine +
-      "&exclude=hourly,daily&appid=e9e74eee0dd11008febcccb305a879fe"
-    fetch(fetchText, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error))
-    console.log("merge")
   }, [])
 
   return (
     <div className="App">
-      <div>af</div>
+      <Navbar></Navbar>
+      <Content date={date} wait={wait}></Content>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/Componente/Footer.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react"
+
+export default function Footer(props) {
+  return (
+    <div className="Footer">
+      <div>Popa George Catalin</div>
+    </div>
+  )
+}
